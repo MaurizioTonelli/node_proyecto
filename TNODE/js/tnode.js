f0f5ac87f0d0262c0102e98eb53fce6013a3d3eb
Maurizio Tonelli <mauriziotonelli@hotmail.com>
2020-05-31 11:40:24 -0500

    Add logout

---
+++ TNODE/js/tnode.js
@@ -0,0 +1,7 @@
+const logout_btn = document.getElementById("logout-btn");
+logout_btn.addEventListener("click", logout);
+
+function logout(){
+    localStorage.removeItem("token");
+    window.location.href="login.html";
+}
