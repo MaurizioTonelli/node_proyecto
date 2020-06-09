80480b5b8c07906df99e13d8746f1894e409b68f
Miguel Angel <miguela_nunez@outlook.com>
2020-06-09 17:58:01 -0500

    Added favicon and made new validations

--- docs/js/tnode.js
+++ docs/js/tnode.js
@@ -60,7 +60,7 @@ function onFormSearch() {
   loadEmployees(search);
 }
 
-function onFormShowAll(){
+function onFormShowAll() {
   document.getElementById("search").value = "";
   loadEmployees();
 }
@@ -76,7 +76,9 @@ function onFormSubmit() {
 }
 function validateEmail(email) {
   var re = /^(?:[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
-  return re.test(email);
+  if (re.test(email)) {
+    return email;
+  }
 }
 function readFormData() {
   var formData = {};
--- docs/js/validations.js
+++ docs/js/validations.js
@@ -1,16 +1,16 @@
 // TNode Validations
-bootstrapValidate("#email", "email|required:Enter a valid E-Mail!");
-bootstrapValidate(
-  "#phone",
-  "numeric|required:Please only enter numeric characters!"
-);
+bootstrapValidate("#email", "email:Enter a valid E-Mail!");
+bootstrapValidate("#email", "required:This field is required");
+bootstrapValidate("#phone", "numeric:Please only enter numeric characters!");
+bootstrapValidate("#phone", "required:This field is required");
 bootstrapValidate(
   "#search",
-  "alphanum|required:Please only enter alphanumeric characters!"
+  "alphanum:Please only enter alphanumeric characters!"
 );
-
+bootstrapValidate("#search", "required:This field is required!");
 bootstrapValidate("#first_name", "required:This field is required!");
-bootstrapValidate("#last_name_name", "required:This field is required!");
+bootstrapValidate("#last_name", "required:This field is required!");
+bootstrapValidate("#address", "required:This field is required!");
 
 // Login Validations
 bootstrapValidate("#input-username", "required:This field is required!");
