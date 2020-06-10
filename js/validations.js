f7fc12d849471b564dc7c877f9d8f6b4d8970b2f
Maurizio Tonelli <mauriziotonelli@hotmail.com>
2020-06-10 15:42:13 -0500

    Revert to old validations.js

--- js/validations.js
+++ js/validations.js
@@ -1,41 +1,35 @@
-var email = true;
-var phone = true;
-window.onload = checkFields;
-
-function checkFields() {
-  if (!email || !phone) {
-    document
-      .getElementById("employee-submit")
-      .setAttribute("disabled", "disabled");
-  } else {
-    if (document.getElementById("employee-submit").hasAttribute("disabled")) {
-      document.getElementById("employee-submit").removeAttribute("disabled");
-    }
+var email = false;
+var phone = false;
+function checkFields(){
+  if(!email || !phone){
+    document.getElementById("employee-submit").setAttribute("disabled", "disabled");
+  }else{
+    document.getElementById("employee-submit").removeAttribute("disabled");
   }
 }
 
 // TNode Validations
-bootstrapValidate("#email", "email:Enter a valid E-Mail!", function (isValid) {
-  if (isValid) {
+bootstrapValidate("#email", "email:Enter a valid E-Mail!", function(isValid){
+  if(isValid){
     email = true;
-    checkFields();
-  } else {
+  }else{
     email = false;
   }
+  checkFields();
 });
-bootstrapValidate(
-  "#phone",
-  "numeric:Please only enter numeric characters!",
-  function (isValid) {
-    if (isValid) {
-      phone = true;
-      checkFields();
-    } else {
-      phone = false;
-    }
+bootstrapValidate("#phone", "numeric:Please only enter numeric characters!", function(isValid){
+  if(isValid){
+    phone = true;
+  }else{
+    phone = false;
   }
-);
+  checkFields();
+});
 bootstrapValidate(
   "#search",
   "alphanum:Please only enter alphanumeric characters!"
 );
+
+// Login Validations
+bootstrapValidate("#input-username", "required:This field is required!");
+bootstrapValidate("#input-password", "required:This field is required!");
